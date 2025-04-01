901367681fd7f66969a6a7953c39f82c3130d3f7
meribelcollin <mcollin@uvm.edu>
2025-04-01 10:25:23 -0400

    Update datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
+// project date
 date_str = "2022-03-17 10:45:30"
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+// date formatting
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
 print(formatted_date)
